45bf8690ba6e68770fd798538f00a3fca526a1ac
rishal-ERPGULF <rishal@htsqatar.com>
2023-10-13 18:52:49 +0530

    tanstack query added

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,17 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import router from "./Routes/Routes";
 import { Toaster } from "@/components/ui/toaster";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark">
       <Provider store={store}>
-        <RouterProvider router={router} />
-        <Toaster />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <Toaster />
+        </QueryClientProvider>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
